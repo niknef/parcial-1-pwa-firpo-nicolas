23af1ec11bb717ad508eecc555a4ff603a664734
Nicolas Firpo <131892657+niknef@users.noreply.github.com>
2023-11-20 17:50:25 -0300

    feat add cards with personajes

--- main.js
+++ main.js
@@ -3,6 +3,25 @@ function fetchPersonaje(){
     .then(response => response.json())
     .then((data) => {
         console.log(data);
+        const personajes = data.results;
+        console.log(personajes);
+        const container = document.querySelector('#container');
+        let html = '';
+        personajes.forEach(personaje => {
+        html += `
+            <div class="col">
+                <div class="card h-100">
+                        <img src="${personaje.image}" class="card-img-top" alt="${personaje.name}">
+                    <div class="card-body">
+                        <h5 class="card-title">${personaje.name}</h5>
+                        <p class="card-text">${personaje.species}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+});
+container.innerHTML = html;
         
     });
 };
